6faa23e95ae4f256aaa9643b82e0e40efb6386fb
Habatakurikei <daicom0204@gmail.com>
2025-03-17 13:51:18 +0100

    corrected version to 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="monju",
-    version="0.2.0",
+    version="0.2.1",
     author="Daisuke Yamaguchi",
     author_email="daicom0204@gmail.com",
     description="A python library for brainstorming by multiple LLMs.",
